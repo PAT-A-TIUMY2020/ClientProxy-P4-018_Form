d5f78afbd98ae3b9574245e55bdb1791d6734254
RamadhanFN <62781418+RamadhanFN@users.noreply.github.com>
2020-10-29 11:48:38 +0700

    Function Penjumlahan

--- ClientProxy-P4-018_Form/Form1.cs
+++ ClientProxy-P4-018_Form/Form1.cs
@@ -12,9 +12,21 @@ namespace ClientProxy_P4_018_Form
 {
     public partial class Form1 : Form
     {
+        ServiceReference1.MatematikaClient obj = new ServiceReference1.MatematikaClient();
+        ServiceReference1.Koordinat a = new ServiceReference1.Koordinat();
+        ServiceReference1.Koordinat b = new ServiceReference1.Koordinat();
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int a, b, c;
+            a = Convert.ToInt32(textBox1.Text);
+            b = Convert.ToInt32(textBox2.Text);
+            c = a + b;
+            textBox9.Text = c.ToString();
         }
     }
 }
